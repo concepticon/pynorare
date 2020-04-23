77a64221ca7a9964f37744bfd6afe434b6a5b190
LinguList <mattis.list@posteo.de>
2020-04-23 16:57:47 +0200

    update norare commands

---
+++ src/pynorare/commands/show.py
@@ -0,0 +1,34 @@
+"""
+Show a dataset with its columns etc.
+"""
+from tabulate import tabulate
+from pynorare.norare import NoRaRe
+
+def register(parser):
+    parser.add_argument(
+            'dataset',
+            nargs=1,
+            help='select your dataset',
+            type=str)
+    parser.add_argument(
+            'columns',
+            nargs='*',           
+            help='show columns',
+            type=str
+            )
+    parser.add_argument(
+            '--items',
+            default=20,
+            help='define number of items',
+            type=int
+            )
+
+def run(args):
+    norare = NoRaRe(repos=args.norarepo)
+    if not args.columns:
+        columns = norare.get_dataset(args.dataset[0])[1]
+    else:
+        columns = args.columns
+    table = norare.get_columns(args.dataset[0], *columns)
+    print(tabulate(table[:args.items], headers=['ID', 'RATING'], tablefmt='pipe'))
+        
---
+++ src/pynorare/norare.py
@@ -0,0 +1,50 @@
+from pathlib import Path
+from csvw.metadata import TableGroup
+from tqdm import tqdm
+from pynorare import log
+
+
+class NoRaRe():
+
+    def __init__(self, repos=None):
+
+        self.repos = Path(repos)
+        self.datasets = {}
+
+    def load_datasets(self):
+        for dataset in tqdm(sorted(
+                self.repos.joinpath('concept_set_meta').glob('*')),
+                desc='loading datasets'):
+            idx = dataset.name
+            if dataset.joinpath(idx+'.tsv').exists() and dataset.joinpath(
+                    idx+'.tsv-metadata.json').exists():
+                self.get_dataset(dataset)
+
+        log.info('loaded datasets')
+
+    def get_dataset(self, dataset):
+        if dataset in self.datasets:
+            return self.datasets[dataset]
+        else:
+            tbg = TableGroup.from_file(
+                    self.repos.joinpath(
+                        'concept_set_meta',
+                        dataset,
+                        dataset+'.tsv-metadata.json').as_posix())
+            self.datasets[dataset] = (
+                    list(tbg.tabledict[dataset+'.tsv']),
+                    [c.name for c in
+                        tbg.tabledict[dataset+'.tsv'].tableSchema.columns]
+                    )
+            return self.datasets[dataset]
+
+    def get_columns(self, dataset, *columns):
+        out = []
+
+        for row in self.get_dataset(dataset)[0]:
+            out += [[row[h] for h in columns]]
+        return out
+
+
+    def __iter__(self):
+        return iter(self.norms)
